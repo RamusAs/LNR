ff3bb93255f44fdc5da5f61dc72baa9f74623ece
Ramus ASSOGBA <ramus@sharemat.fr>
2026-02-08 13:16:18 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const links = document.querySelectorAll(".nav__item");
+const nav = document.querySelector("nav");
+const navL = document.querySelector(".nav__list");
+const icons = document.querySelector("#icons");
 
+icons.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
